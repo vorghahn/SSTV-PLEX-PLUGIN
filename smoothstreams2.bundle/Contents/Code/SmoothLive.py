026ea1c03a3f0112c1ab76eb224944f84c32bc6e
vorghahn <vaughan.towers@gmail.com>
2017-08-22 11:50:58 +1200

    added file close
    Also uncommented sections for use

--- smoothstreams2.bundle/Contents/Code/SmoothLive.py
+++ smoothstreams2.bundle/Contents/Code/SmoothLive.py
@@ -40,36 +40,36 @@ from flask import Flask, Response, request, jsonify, abort
 app = Flask(__name__)
 
 
-# config = {
-#     'bindAddr': os.environ.get('SSTV_BINDADDR') or '',
-#     'sstvProxyURL': os.environ.get('SSTV_PROXY_URL') or 'http://localhost',
-#     'tunerCount': os.environ.get('SSTV_TUNER_COUNT') or 6,  # number of tuners to use for sstv
-# }
-#
-#
-# @app.route('/discover.json')
-# def discover():
-#     return jsonify({
-#         'FriendlyName': 'sstvProxy',
-#         'ModelNumber': 'HDTC-2US',
-#         'FirmwareName': 'hdhomeruntc_atsc',
-#         'TunerCount': int(config['tunerCount']),
-#         'FirmwareVersion': '20150826',
-#         'DeviceID': '12345678',
-#         'DeviceAuth': 'test1234',
-#         'BaseURL': '%s' % config['sstvProxyURL'],
-#         'LineupURL': '%s/lineup.json' % config['sstvProxyURL']
-#     })
-#
-#
-# @app.route('/lineup_status.json')
-# def status():
-#     return jsonify({
-#         'ScanInProgress': 0,
-#         'ScanPossible': 1,
-#         'Source': "Cable",
-#         'SourceList': ['Cable']
-#     })
+config = {
+    'bindAddr': os.environ.get('SSTV_BINDADDR') or '',
+    'sstvProxyURL': os.environ.get('SSTV_PROXY_URL') or 'http://localhost',
+    'tunerCount': os.environ.get('SSTV_TUNER_COUNT') or 6,  # number of tuners to use for sstv
+}
+
+
+@app.route('/discover.json')
+def discover():
+    return jsonify({
+        'FriendlyName': 'sstvProxy',
+        'ModelNumber': 'HDTC-2US',
+        'FirmwareName': 'hdhomeruntc_atsc',
+        'TunerCount': int(config['tunerCount']),
+        'FirmwareVersion': '20150826',
+        'DeviceID': '12345678',
+        'DeviceAuth': 'test1234',
+        'BaseURL': '%s' % config['sstvProxyURL'],
+        'LineupURL': '%s/lineup.json' % config['sstvProxyURL']
+    })
+
+
+@app.route('/lineup_status.json')
+def status():
+    return jsonify({
+        'ScanInProgress': 0,
+        'ScanPossible': 1,
+        'Source': "Cable",
+        'SourceList': ['Cable']
+    })
 
 
 @app.route('/lineup.json')
@@ -126,6 +126,7 @@ def lineup():
                            'GuideName': str(channelNum) + " " + channelName,
                            'URL': pipeUrl
                            })
+    file.close()
         # print ({'GuideNumber': channelNum,
         #                    'GuideName': str(channelNum) + channelName,
         #                    'URL': url
@@ -136,17 +137,17 @@ def lineup():
     #                       'URL':"http://dnaw1.smoothstreams.tv:9100/viewms/ch01q1.stream/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9OC8yMC8yMDE3IDc6NDU6MDIgUE0maGFzaF92YWx1ZT1FanNiNVFmeEFNb211cVN6Zkl3c3JBPT0mdmFsaWRtaW51dGVzZpZD12aWV3bXMtMTUzNTk==="
     #                       })
 
-    # return jsonify(lineup)
+    return jsonify(lineup)
 
 
-# @app.route('/lineup.post')
-# def lineup_post():
-#     return ''
-#
-#
-# if __name__ == '__main__':
-#     http = WSGIServer((config['bindAddr'], 5004), app.wsgi_app)
-#     http.serve_forever()
+@app.route('/lineup.post')
+def lineup_post():
+    return ''
 
-lineup()
+
+if __name__ == '__main__':
+    http = WSGIServer((config['bindAddr'], 5004), app.wsgi_app)
+    http.serve_forever()
+
+# lineup()
 
